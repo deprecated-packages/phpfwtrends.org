eadbfc69349070d8653ebff17a349f72816257b3
TomasVotruba <tomas.vot@gmail.com>
2021-03-02 00:42:02 +0100

    remove autowire array

--- src/HttpKernel/PhpFwTrendsKernel.php
+++ src/HttpKernel/PhpFwTrendsKernel.php
@@ -9,7 +9,6 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
-use Symplify\AutowireArrayParameter\DependencyInjection\CompilerPass\AutowireArrayParameterCompilerPass;
 
 final class PhpFwTrendsKernel extends Kernel
 {
@@ -23,10 +22,5 @@ final class PhpFwTrendsKernel extends Kernel
     protected function configureRoutes(RoutingConfigurator $routingConfigurator): void
     {
         $routingConfigurator->import(__DIR__ . '/../../config/routes.php');
-    }
-
-    protected function build(ContainerBuilder $containerBuilder): void
-    {
-        $containerBuilder->addCompilerPass(new AutowireArrayParameterCompilerPass());
     }
 }
