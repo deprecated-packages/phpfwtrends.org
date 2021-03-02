6b479f686953e34e1c9a225099a1b312535a715e
TomasVotruba <tomas.vot@gmail.com>
2021-03-02 11:14:03 +0100

    try adding class

---
+++ src/Kernel.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TomasVotruba\PhpFwTrends;
+
+// this is a real mystery; symfony needs this file to be here for some unknown crazy reason :/
+// spend 4 hours on this one
+// fake kernel, to make CI random fail pass, see https://github.com/TomasVotruba/phpfwtrends.org/runs/2012166446
+final class Kernel
+{
+
+}
