398f19b46ae3a36d30034ee695662c4fcf2769f8
TomasVotruba <tomas.vot@gmail.com>
2021-03-02 01:17:22 +0100

    misc

--- src/Controller/FrameworkController.php
+++ src/Controller/FrameworkController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symplify\PackageBuilder\Parameter\ParameterProvider;
+use TomasVotruba\PhpFwTrends\Exception\ShouldNotHappenException;
 use TomasVotruba\PhpFwTrends\ValueObject\Option;
 use TomasVotruba\PhpFwTrends\ValueObject\RouteName;
 
@@ -21,11 +22,26 @@ final class FrameworkController extends AbstractController
     #[Route(path: 'framework/{frameworkName}', name: RouteName::FRAMEWORK)]
     public function __invoke(string $frameworkName): Response
     {
-        $phpFrameworkTrends = $this->parameterProvider->provideArrayParameter(Option::PHP_FRAMEWORK_TRENDS);
+        $frameworkTrend = $this->matchFrameworkTrend($frameworkName);
+        if ($frameworkTrend === []) {
+            throw new ShouldNotHappenException($frameworkTrend);
+        }
 
-        return $this->render('homepage/methodology.twig', [
+        return $this->render('homepage/framework.twig', [
             'title' => 'PHP Framework Trends',
-            'framework_trend' => $phpFrameworkTrends[$frameworkName],
+            'framework_trend' => $frameworkTrend,
         ]);
+    }
+
+    private function matchFrameworkTrend(string $frameworkName): array
+    {
+        $phpFrameworkTrends = $this->parameterProvider->provideArrayParameter(Option::PHP_FRAMEWORK_TRENDS)['vendors'];
+        foreach ($phpFrameworkTrends as $frameworkTrend) {
+            if ($frameworkTrend['vendor_key'] === $frameworkName) {
+                return $frameworkTrend;
+            }
+        }
+
+        return [];
     }
 }
