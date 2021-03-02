52db3eb83bb625e547bacc6bbb81a5561490af0b
TomasVotruba <tomas.vot@gmail.com>
2021-03-02 11:02:56 +0100

    bump

--- config/config.php
+++ config/config.php
@@ -9,9 +9,9 @@ use Symplify\PackageBuilder\Console\Style\SymfonyStyleFactory;
 use Symplify\SmartFileSystem\SmartFileSystem;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import(__DIR__ . '/parameters.php');
     $containerConfigurator->import(__DIR__ . '/_data/generated/*');
     $containerConfigurator->import(__DIR__ . '/packages/*');
-    $containerConfigurator->import(__DIR__ . '/parameters.php');
 
     $services = $containerConfigurator->services();
 
@@ -20,7 +20,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ->autowire()
         ->autoconfigure();
 
-    $services->load('TomasVotruba\\PhpFwTrends\\', __DIR__ . '/../src')
+    $services->load('TomasVotruba\PhpFwTrends\\', __DIR__ . '/../src')
         ->exclude([__DIR__ . '/../src/HttpKernel', __DIR__ . '/../src/ValueObject']);
 
     $services->set(SymfonyStyleFactory::class);
