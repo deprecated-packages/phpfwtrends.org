48aeb51ee6cb7b5442fb7dff319cf8de82197417
TomasVotruba <tomas.vot@gmail.com>
2021-03-02 10:56:07 +0100

    no patches

--- config/bundles.php
+++ config/bundles.php
@@ -1,12 +1,11 @@
 <?php
 
-declare(strict_types=1);
-
 return [
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symplify\SymfonyStaticDumper\SymfonyStaticDumperBundle::class => ['all' => true],
-    Symplify\PhpConfigPrinter\Bundle\PhpConfigPrinterBundle::class => ['all' => true],
     Symplify\EasyHydrator\EasyHydratorBundle::class => ['all' => true],
     Symplify\SimplePhpDocParser\Bundle\SimplePhpDocParserBundle::class => ['all' => true],
+    Symplify\PhpConfigPrinter\Bundle\PhpConfigPrinterBundle::class => ['all' => true],
 ];
