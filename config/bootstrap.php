671ae6523f05352d0c3050d99ff7ad92d5ba7936
TomasVotruba <tomas.vot@gmail.com>
2021-03-02 10:59:01 +0100

    misc

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -6,21 +6,22 @@ use Symfony\Component\Dotenv\Dotenv;
 
 // Load cached env vars if the .env.local.php file exists
 // Run "composer dump-env prod" to create it (requires symfony/flex >=1.2)
-if (is_array(
-        $env = @include dirname(__DIR__) . '/.env.local.php'
-    ) && (! isset($env['APP_ENV']) || $env['APP_ENV'] === ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? $env['APP_ENV']))) {
-    foreach ($env as $k => $v) {
-        $_ENV[$k] = $_ENV[$k] ?? (isset($_SERVER[$k]) && strpos($k, 'HTTP_') !== 0 ? $_SERVER[$k] : $v);
-    }
+if (is_array($env = @include dirname(__DIR__) . '/.env.local.php')) {
+    $_SERVER += $env;
+    $_ENV += $env;
 } else {
     // load all the .env files
-    (new Dotenv(false))->loadEnv(dirname(__DIR__) . '/.env');
+    (new Dotenv())->loadEnv(dirname(__DIR__) . '/.env');
 }
 
-$_SERVER += $_ENV;
 $_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = $_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? null ?: 'dev';
 $_SERVER['APP_DEBUG'] = $_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? $_SERVER['APP_ENV'] !== 'prod';
 $_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] = (int) $_SERVER['APP_DEBUG'] || filter_var(
     $_SERVER['APP_DEBUG'],
     FILTER_VALIDATE_BOOLEAN
 ) ? '1' : '0';
+
+if (isset($_COOKIE['XDEBUG_TRACE']) && ! empty($_ENV['DEBUG_COOKIE']) && $_COOKIE['XDEBUG_TRACE'] === $_ENV['DEBUG_COOKIE']) {
+    $_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] = 1;
+    $_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = 'dev';
+}
