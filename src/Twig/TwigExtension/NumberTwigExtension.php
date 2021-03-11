6a855ea8b7124f8fdcc44eef0cfadcb10daa0ae8
TomasVotruba <tomas.vot@gmail.com>
2021-03-11 01:46:10 +0100

    millions

--- src/Twig/TwigExtension/NumberTwigExtension.php
+++ src/Twig/TwigExtension/NumberTwigExtension.php
@@ -15,23 +15,13 @@ final class NumberTwigExtension extends AbstractExtension
     public function getFilters(): array
     {
         $twigFilters = [];
-        $twigFilters[] = new TwigFilter('millions', function (int $number): string {
-            if ($number > 10 ** 5) {
-                return $this->formatNumber($number / (10 ** 6)) . ' M';
-            }
-
-            if ($number > 10 ** 2) {
-                return $this->formatNumber($number / (10 ** 3)) . ' K';
-            }
-
-            return $this->formatNumber($number);
-        });
+        $twigFilters[] = new TwigFilter('thousands', fn(int $number): string => $this->formatNumber($number / (10 ** 3)) . ' K');
 
         return $twigFilters;
     }
 
     private function formatNumber(float|int $number): string
     {
-        return number_format($number, 1, '.', ' ');
+        return number_format($number, 0, '.', ' ');
     }
 }
